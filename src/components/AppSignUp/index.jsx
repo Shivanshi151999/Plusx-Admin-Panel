05e9efaf084622ab3286e7e6033d4f04f2429d71
Ajo-shunyaekai <Ajo@shunyaekai.tech>
2024-10-24 11:23:13 +0530

    added: portable charger and pick and drop booking history in rider details

--- src/components/AppSignUp/index.jsx
+++ src/components/AppSignUp/index.jsx
@@ -7,6 +7,7 @@ import DetailsBookingHistory from '../SharedComponent/Details/DeatilsBookingHist
 import DetailsVehicleList from '../SharedComponent/Details/DetailsVehicleList'
 import { getRequestWithToken } from '../../api/Requests';
 import { useParams } from 'react-router-dom';
+import moment from 'moment';
 
 
 const RiderDetails = () => {
@@ -14,6 +15,16 @@ const RiderDetails = () => {
   const [riderDetails, setRiderDetails] = useState()
   const [riderAddressList, setRiderAddressList] = useState([])
   const [vehicleList, setVehicleList] = useState([])
+  const [portableChargerBookings, setPortableChargerBookings] = useState([])
+  const [pickAndDropBookings, setPickAndDropBookings] = useState([])
+
+  const portableChargerHeaders = [
+    'ID', 'Service Name', 'Service Type', 'Price', 'Date & Time', 'Status', 'Action'
+  ];
+
+  const pickAndDropHeaders = [
+    'ID',  'Price', 'Date & Time', 'Status', 'Action'
+  ];
 
   const fetchDetails = () => {
     const obj = {
@@ -27,6 +38,8 @@ const RiderDetails = () => {
           setRiderDetails(response?.data || {});  
           setRiderAddressList(response?.data?.riderAddress)
           setVehicleList(response?.data?.riderVehicles)
+          setPortableChargerBookings(response?.data?.portableChargerBookings)
+          setPickAndDropBookings(response?.data?.pickAndDropBookings)
         } else {
             console.log('error in rider-details API', response);
         }
@@ -42,7 +55,33 @@ const RiderDetails = () => {
       <DetailsSection sectionDetails = {riderDetails}/>
       <DetailsList addressList = {riderAddressList}/>
       <DetailsVehicleList vehicleList = {vehicleList} />
-      <DetailsBookingHistory />
+ 
+      <DetailsBookingHistory
+        title="Portable Charger Booking History"
+        headers={portableChargerHeaders}
+        bookingData={portableChargerBookings.map((booking) => ({
+          id: booking.booking_id,
+          service_name: booking.service_name,
+          service_type: booking.service_type,
+          price: booking.service_price || '0', 
+          datetime:  moment(booking.created_at).format('DD MMM YYYY h:mm A'),
+          status: booking.order_status,
+        }))}
+        bookingType="portableCharger"
+      />
+
+       <DetailsBookingHistory
+        title="Pick and Drop Booking History"
+        headers={pickAndDropHeaders}
+        bookingData={pickAndDropBookings.map((booking) => ({
+          id: booking.request_id,
+          
+          price: booking.price || '0',
+          datetime: moment(booking.created_at).format('DD MMM YYYY h:mm A') ,
+          status: booking.status,
+        }))}
+        bookingType="pickAndDrop"
+      />
     </div>
   )
 }
--- src/components/SharedComponent/Details/DeatilsBookingHistory.jsx
+++ src/components/SharedComponent/Details/DeatilsBookingHistory.jsx
@@ -3,56 +3,36 @@ import styles from './details.module.css'
 import Eye from '../../../assets/images/ViewEye.svg'
 import Delete from '../../../assets/images/Delete.svg'
 
-const DeatilsBookingHistory = () => {
-  const addresses = [
-    {
-      id: "123456",
-      service_name: "E-POD",
-      service_type: "on-demand",
-      price: "AED 123",
-      datetime: "11 OCT 2024 11:15 AM",
-      status: "POP Picked Up"
 
-    },
-    {
-      id: "123456",
-      customer_name: "Husaain ji",
-      service_name: "E-POD",
-      price: "AED 123",
-      datetime: "11 OCT 2024 11:15 AM",
-      status: "POP Picked Up"
 
-    },
-  ];
-
+const DeatilsBookingHistory = ({ title, headers, bookingData, bookingType }) => {
   return (
     <div className={styles.addressListContainer}>
-      <span className={styles.sectionTitle}>Booking History</span>
+      <span className={styles.sectionTitle}>{title}</span>
       <table className={`table ${styles.customTable}`}>
         <thead>
           <tr>
-            <th>ID</th>
-            <th>Service Name</th>
-            <th>Service Type</th>
-            <th>Price</th>
-            <th>Date & Time</th>
-            <th>Status</th>
-            <th>Action</th>
+            {headers.map((header, index) => (
+              <th key={index}>{header}</th>
+            ))}
           </tr>
         </thead>
         <tbody>
-          {addresses.map((address, index) => (
+          {bookingData.map((booking, index) => (
             <tr key={index}>
-              <td>{address.id}</td>
-              <td>{address.service_name}</td>
-              <td>{address.service_type}</td>
-              <td>{address.price}</td>
-              <td>{address.datetime}</td>
-              <td>{address.status}</td>
+              <td>{booking.id}</td>
+              {bookingType === 'portableCharger' ? (
+                <>
+                  <td>{booking.service_name}</td>
+                  <td>{booking.service_type}</td>
+                </>
+              ) : null}
+              <td>{booking.price}</td>
+              <td>{booking.datetime}</td>
+              <td>{booking.status}</td>
               <td>
                 <div className={styles.editContent}>
                   <img src={Eye} alt="Eye" />
-
                 </div>
               </td>
             </tr>
@@ -62,5 +42,6 @@ const DeatilsBookingHistory = () => {
     </div>
   );
 };
+
 
 export default DeatilsBookingHistory;
