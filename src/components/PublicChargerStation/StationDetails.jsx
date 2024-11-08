98e8e888b32518c63441caaacbd9aafe6a62c8af
Ajo-shunyaekai <Ajo@shunyaekai.tech>
2024-11-08 17:47:58 +0530

    modified: station details

--- src/components/PublicChargerStation/StationDetails.jsx
+++ src/components/PublicChargerStation/StationDetails.jsx
@@ -21,51 +21,6 @@ const statusMapping = {
   'C': 'Cancel'
 };
 
-// const formatTime = (timeStr) => {
-//   if (timeStr === "Closed") return "Closed";
-//   const [start, end] = timeStr?.split('-');
-//   const format12Hour = (time) => {
-//     const [hour, minute] = time?.split(':');
-//     const date = new Date();
-//     date.setHours(hour);
-//     date.setMinutes(minute);
-//     return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-//   };
-//   return `${format12Hour(start)} - ${format12Hour(end)}`;
-// };
-
-// const getFormattedOpeningHours = (details) => {
-//   if (details?.always_open === 1) {
-//     return "Always Open";
-//   }
-
-//   if (!details?.open_days || !details?.open_timing) {
-//     return "No opening hours available";
-//   }
-
-//   const days = details?.open_days.split(', ');
-//   const timings = details?.open_timing.split(', ').map(formatTime);
-//   let formattedHours = [];
-
-//   for (let i = 0; i < days?.length; i++) {
-//     let startDay = days[i];
-//     let currentTiming = timings[i];
-//     let j = i;
-
-//     while (j < days?.length - 1 && timings[j + 1] === currentTiming) {
-//       j++;
-//     }
-
-//     const dayRange = startDay + (i === j ? "" : `-${days[j]}`);
-//     formattedHours.push(`${dayRange}: ${currentTiming}`);
-//     i = j;
-//   }
-
-//   return formattedHours.join("\n");
-// };
-
-
-
 
 const formatTime = (timeStr) => {
   if (timeStr === "Closed") return "Closed";
@@ -127,10 +82,6 @@ const getFormattedOpeningHours = (details) => {
 
   return formattedOpeningHours.join(', ');
 };
-
-
-
-
 
 
 const StationDetails = () => {
