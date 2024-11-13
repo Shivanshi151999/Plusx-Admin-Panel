62c0229b47e22e239f1f657a17ba65139d087db0
Shivanshi151999 <118881439+Shivanshi151999@users.noreply.github.com>
2024-11-13 16:01:12 +0530

    changes in admin apnel

--- src/components/Coupon/CouponList.jsx
+++ src/components/Coupon/CouponList.jsx
@@ -14,7 +14,7 @@ const dynamicFilters = [
 ]
 
 const CouponList = () => {
-    const userDetails = JSON.parse(sessionStorage.getItem('userDetails')); 
+    const userDetails = JSON.parse(sessionStorage.getItem('userDetails'));
     const navigate = useNavigate()
     const [carList, setCarList] = useState([])
     const [currentPage, setCurrentPage] = useState(1);
@@ -23,29 +23,29 @@ const CouponList = () => {
     const [refresh, setRefresh] = useState(false)
     const searchTerm = [
         {
-            label: 'search', 
-            name: 'search_text', 
+            label: 'search',
+            name: 'search_text',
             type: 'text'
         }
     ]
 
     const addButtonProps = {
-        heading: "Add Coupon", 
+        heading: "Add Coupon",
         link: "/add-coupon"
     };
 
     const fetchList = (page, appliedFilters = {}) => {
         const obj = {
-            userId : userDetails?.user_id,
-            email : userDetails?.email,
-            page_no : page,
+            userId: userDetails?.user_id,
+            email: userDetails?.email,
+            page_no: page,
             ...appliedFilters,
         }
 
-        postRequestWithToken('coupon-list', obj, async(response) => {
+        postRequestWithToken('coupon-list', obj, async (response) => {
             if (response.code === 200) {
                 setCarList(response?.data)
-                setTotalPages(response?.total_page || 1); 
+                setTotalPages(response?.total_page || 1);
             } else {
                 // toast(response.message, {type:'error'})
                 console.log('error in coupon-list api', response);
@@ -55,8 +55,8 @@ const CouponList = () => {
 
     useEffect(() => {
         if (!userDetails || !userDetails.access_token) {
-            navigate('/login'); 
-            return; 
+            navigate('/login');
+            return;
         }
         fetchList(currentPage, filters);
     }, [currentPage, filters, refresh]);
@@ -65,25 +65,33 @@ const CouponList = () => {
         setCurrentPage(pageNumber);
     };
     const fetchFilteredData = (newFilters = {}) => {
-        setFilters(newFilters);  
-        setCurrentPage(1); 
+        setFilters(newFilters);
+        setCurrentPage(1);
     };
 
     const handleDeleteSlot = (code) => {
         const confirmDelete = window.confirm("Are you sure you want to delete this?");
         if (confirmDelete) {
+<<<<<<< Updated upstream
             const obj = { 
                 userId : userDetails?.user_id,
                 email : userDetails?.email,
                 // coupon_id: couponId ,
                 coupan_code: code
+=======
+            const obj = {
+                userId: userDetails?.user_id,
+                email: userDetails?.email,
+                coupon_id: couponId,
+                // coupon_code: couponCode
+>>>>>>> Stashed changes
             };
             postRequestWithToken('delete-coupan', obj, async (response) => {
                 if (response.code === 200) {
                     toast(response.message, { type: "success" });
                     setTimeout(() => {
                         setRefresh(prev => !prev);
-                    },1000)
+                    }, 1000)
                 } else {
                     toast(response.message, { type: 'error' });
                     console.log('error in electric-bike-delete api', response);
@@ -95,38 +103,38 @@ const CouponList = () => {
     return (
         <div className={styles.electricBikeSection}>
             <ToastContainer />
-         <SubHeader heading = "Coupon List" 
-         addButtonProps={addButtonProps}
-         fetchFilteredData={fetchFilteredData} 
-         dynamicFilters={dynamicFilters} filterValues={filters}
-         searchTerm = {searchTerm}
-         />
-        <List 
-          tableHeaders={["Coupon ID", "Coupon Name", "Coupon Code", "Service Name", "Per User", "Coupon %", "End Date", "Status", "Action"]}
-          listData = {carList}
-          keyMapping={[
-            { key: 'id', label: 'Coupon ID' }, 
-            { key: 'coupan_name', label: 'Coupon Name' },
-            { key: 'coupan_code', label: 'Coupon Code' },
-            { 
-                key: 'booking_for', 
-                label: 'Service Name',  
-            },
-            { key: 'user_per_user', label: 'Per User' },
-            { key: 'coupan_percentage', label: 'Coupon %' },
-            // { key: 'end_date', label: 'End Date' },
-            { key: 'end_date', label: 'End Date', format: (date) => moment(date).format('DD MMM YYYY') },
-            { key: 'status', label: 'Status', format: (status) => (status === "1" ? "Active" : "Inactive") } 
-        ]}
-        pageHeading="Coupon List"
-        onDeleteSlot={handleDeleteSlot}
-          />
-           
-        <Pagination 
-          currentPage={currentPage} 
-          totalPages={totalPages} 
-          onPageChange={handlePageChange} 
-        />
+            <SubHeader heading="Coupon List"
+                addButtonProps={addButtonProps}
+                fetchFilteredData={fetchFilteredData}
+                dynamicFilters={dynamicFilters} filterValues={filters}
+                searchTerm={searchTerm}
+            />
+            <List
+                tableHeaders={["Coupon ID", "Coupon Name", "Coupon Code", "Service Name", "Per User", "Coupon %", "End Date", "Status", "Action"]}
+                listData={carList}
+                keyMapping={[
+                    { key: 'id', label: 'Coupon ID' },
+                    { key: 'coupan_name', label: 'Coupon Name' },
+                    { key: 'coupan_code', label: 'Coupon Code' },
+                    {
+                        key: 'booking_for',
+                        label: 'Service Name',
+                    },
+                    { key: 'user_per_user', label: 'Per User' },
+                    { key: 'coupan_percentage', label: 'Coupon %' },
+                    // { key: 'end_date', label: 'End Date' },
+                    { key: 'end_date', label: 'End Date', format: (date) => moment(date).format('DD MMM YYYY') },
+                    { key: 'status', label: 'Status', format: (status) => (status === "1" ? "Active" : "Inactive") }
+                ]}
+                pageHeading="Coupon List"
+                onDeleteSlot={handleDeleteSlot}
+            />
+
+            <Pagination
+                currentPage={currentPage}
+                totalPages={totalPages}
+                onPageChange={handlePageChange}
+            />
         </div>
     );
 };
