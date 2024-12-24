47b60073661b68b0ea7dde4de5ea1d4a8a656076
rishabhshunyaekai <rishabhkumar@shunyaekai.tech>
2024-12-24 11:10:14 +0530

    Revert "resolved conflicts"
    This reverts commit faf6bbcd20723e604532222613d8567c9cc89910.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import Router from "./routes/Router";
-import MQTT from './utils/mqtt'
 
 function App() {
   return (
     <>
-    <MQTT/>
       <Router />
     </>
   );
